d1701accee14744824245712c6bfada93e4f3e95
mlevental <m.levental@web.de>
2016-01-22 22:36:21 +0100

    moaar tests

--- firstapp/tests.py
+++ firstapp/tests.py
@@ -1,16 +1,16 @@
 from django.test import TestCase
 
 
-class TestIndexPage(TestCase):
-    # def test_get_index_page(self):
-    #     response = self.client.get('')
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertContains(response, "<input type='hidden' name='csrfmiddlewaretoken'")
-    #
-    # def test_post_index_page(self):
-    #     response = self.client.post('')
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertContains(response, "<input type='hidden' name='csrfmiddlewaretoken'")
+class IndexPageTest(TestCase):
+    def test_get_index_page(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<input type='hidden' name='csrfmiddlewaretoken'")
+
+    def test_post_index_page(self):
+        response = self.client.post('')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<input type='hidden' name='csrfmiddlewaretoken'")
 
     def test_send_email(self):
         response = self.client.post('/sendemail', HTTP_X_REQUESTED_WITH='XMLHttpRequest')
