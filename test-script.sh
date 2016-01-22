ab2d46f2aa56bf6f571047b3e4e4308e84917261
mlevental <m.levental@web.de>
2016-01-23 00:31:18 +0100

    shell script added

---
+++ test-script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo ---------------------------- Unit Tests -----------------------------
+python manage.py test firstapp.tests.test_ut
+echo ------------------------- Integration Tests -------------------------
+python manage.py test firstapp.tests.test_it
