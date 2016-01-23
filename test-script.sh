724ea805ebe82216b98d0c1293f831f362e0854e
mlevental <m.levental@web.de>
2016-01-23 14:27:04 +0100

    coveralls integration

--- test-script.sh
+++ test-script.sh
@@ -4,3 +4,5 @@ echo ----------------------------- Unit Tests -----------------------------
 python manage.py test firstapp.tests.test_ut
 echo ------------------------- Integration Tests --------------------------
 python manage.py test firstapp.tests.test_it
+echo ------------------------- Coverage --------------------------
+coverage run --source=firstapp manage.py test firstapp
