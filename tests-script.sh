faf7afa8affd2462fc7844ff45a4dc32c4e57eae
mlevental <m.levental@web.de>
2016-01-23 15:52:14 +0100

    better test-script formatting and more omits

--- tests-script.sh
+++ tests-script.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 echo ----------------------------- Unit Tests -----------------------------
-coverage run -p --source=firstapp --omit *test_*.py,*__init__.py,*apps.py manage.py test firstapp.tests.test_ut
+coverage run -p --source=. \
+    --omit *test_*.py,*migrations*,*__init__.py,*apps.py,*admin.py,*wsgi.py,*settings.py,manage.py \
+    manage.py test firstapp.tests.test_ut
+
 echo ------------------------- Integration Tests --------------------------
-coverage run -p --source=firstapp --omit *test_*.py,*__init__.py,*apps.py manage.py test firstapp.tests.test_it
+coverage run -p --source=. \
+    --omit *test_*.py,*migrations*,*__init__.py,*apps.py,*admin.py,*wsgi.py,*settings.py,manage.py \
+    manage.py test firstapp.tests.test_it
+    
 echo ---------------------- Combine Coverage Reports ----------------------
 coverage combine
