9ba06a825a15f14583093f691d3975b22178b3db
mlevental <m.levental@web.de>
2016-01-23 14:36:44 +0100

    coverage added, requirement added

--- tests-script.sh
+++ tests-script.sh
@@ -5,3 +5,4 @@ python manage.py test firstapp.tests.test_ut
 echo ------------------------- Integration Tests --------------------------
 python manage.py test firstapp.tests.test_it
 echo ------------------------- Coverage --------------------------
+coverage run --source=firstapp manage.py test firstapp
