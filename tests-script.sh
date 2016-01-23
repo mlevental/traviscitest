75d1f0d78ba5e579d30d5d1cefe27596c1184399
mlevental <m.levental@web.de>
2016-01-23 15:32:26 +0100

    ignore unimportant code from coverage

--- tests-script.sh
+++ tests-script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo ----------------------------- Unit Tests -----------------------------
-coverage run -p --source=firstapp --omit *test_*.py manage.py test firstapp.tests.test_ut
+coverage run -p --source=firstapp --omit *test_*.py,*__init__.py,*apps.py manage.py test firstapp.tests.test_ut
 echo ------------------------- Integration Tests --------------------------
-coverage run -p --source=firstapp --omit *test_*.py manage.py test firstapp.tests.test_it
+coverage run -p --source=firstapp --omit *test_*.py,*__init__.py,*apps.py manage.py test firstapp.tests.test_it
 echo ---------------------- Combine Coverage Reports ----------------------
 coverage combine
