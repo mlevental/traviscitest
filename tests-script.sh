7dce4bf86c81145ede01445675da0e4e4f33ec3b
mlevental <m.levental@web.de>
2016-01-23 14:57:13 +0100

    coverage separate

--- tests-script.sh
+++ tests-script.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 echo ----------------------------- Unit Tests -----------------------------
-python manage.py test firstapp.tests.test_ut
+coverage run --source=firstapp manage.py test firstapp.tests.test_ut
 echo ------------------------- Integration Tests --------------------------
-python manage.py test firstapp.tests.test_it
-echo ------------------------- Coverage --------------------------
-coverage run --source=firstapp manage.py test firstapp
+coverage run --source=firstapp manage.py test firstapp.tests.test_it
