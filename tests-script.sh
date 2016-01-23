d2f5cb91ecfbc9d35b15b0a03fe6c0bcb90693c1
mlevental <m.levental@web.de>
2016-01-23 14:33:56 +0100

    coverage deleted

--- tests-script.sh
+++ tests-script.sh
@@ -5,4 +5,3 @@ python manage.py test firstapp.tests.test_ut
 echo ------------------------- Integration Tests --------------------------
 python manage.py test firstapp.tests.test_it
 echo ------------------------- Coverage --------------------------
-coverage run --source=firstapp manage.py test firstapp
